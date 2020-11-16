fac023522b392a0710973d6cb7aec9514aa3d09f
unknown <j.m.kazimierski@gmail.com>
2020-11-16 12:03:43 +0100

    Code improved, also tests

--- ArithGeo/ArithGeo.py
+++ ArithGeo/ArithGeo.py
@@ -5,46 +5,54 @@ November 2020 Jakub Kazimierski
 
 def ArithGeo(arr):
     '''
-    method check if given sequence is
-    arithmetic or geometric sequence
-    input not contain same symbols
-    also input not contain 0
+    Have the function ArithGeo(arr) take the array 
+    of numbers stored in arr and return the string 
+    "Arithmetic" if the sequence follows an arithmetic pattern 
+    or return "Geometric" if it follows a geometric pattern. 
+    If the sequence doesn't follow either pattern return -1. 
+    An arithmetic sequence is one where the difference between each of the numbers is consistent, 
+    where as in a geometric sequence, each term after the first is multiplied by some constant 
+    or common ratio. Arithmetic example: [2, 4, 6, 8] and Geometric example: [2, 6, 18, 54]. 
+    Negative numbers may be entered as parameters, 0 will not be entered, 
+    and no array will contain all the same elements.
     '''
     try:
-        #those var will define output
+        #Those var will define output
         #default both are True
         isArythmetic = "True"
         isGeometric = "True"
 
-        #bufors for differences in sequences
-        bufArytm = float(arr[1]) - float(arr[0])
-        bufGeo = float(arr[1])/float(arr[0])
+        #Differences in sequences
+        diffArytm = arr[1] - arr[0]
+        sequenceArythmetic = arr[0]
+        
+        #If division output is different than integer
+        #bufGeo will be float
+        diffGeo = arr[1]/arr[0]
+        sequenceGeometric = arr[0]
 
+        #Complexity O(n)
+        for i in arr:
+            #In arythmetic sequence, difference beetween
+            #each element coming up from previous one is const
+            if sequenceArythmetic != i:
+                isArythmetic = "False" 
+            sequenceArythmetic += diffArytm        
 
-
-        #in arythmetic sequence, difference beetween
-        #each element coming up from previous one is const
-        #iterate array till last-1 elem
-        for i in range(0, len(arr)-1):
-            
-            bufLoopAryt = float(arr[i+1]) - float(arr[i])
-            #check if value changes
-            if bufArytm != bufLoopAryt:
-                isArythmetic = "False"
-
-        #in gEometric sequence value of division beetwen 
-        #two coming up elements of sequence is const
-        for j in range(0, len(arr)-1):
-
-            bufLoopGeo = float(arr[j+1])/float(arr[j])
-            #check if value changes
-            if bufLoopGeo != bufGeo:
+            #In geometric sequence value of division beetwen 
+            #two coming up elements of sequence is const
+            if sequenceGeometric != i:
                 isGeometric = "False"
+            sequenceGeometric *= diffGeo    
 
-        if isArythmetic == "True":
+        #First condition assert that sequence will not be 
+        #geometric and arithmetic at the same time
+        if isArythmetic == "True" and isGeometric == "True":
+            return "Input array contains same elements"
+        elif isArythmetic == "True":
             return "Arithmetic"               
         elif isGeometric == "True":
-            return "Geometric"
+            return "Geometric"    
         else:    
             return -1
             
--- ArithGeo/test_ArithGeo.py
+++ ArithGeo/test_ArithGeo.py
@@ -8,54 +8,63 @@ import ArithGeo
 
 class test_ArithGeo(unittest.TestCase):
     '''
-    class contains unittests for
+    Class contains unittests for
     ArithGeo.py
     '''
 
     #region Unittests
-    def test_AithmeticOutput(self):
+    def test_ArithmeticOutput(self):
         '''
-        check if for arithmetic seq
-        output is "Arithmetic"
+        Checks if for arithmetic sequence
+        output is "Arithmetic".
         '''
         output = ArithGeo.ArithGeo([1,2,3,4])
         self.assertEqual(output, "Arithmetic")
 
     def test_GeometricOutput(self):
         '''
-        check if output for geometric seq
-        is "Geometric"
+        Checks if output for geometric sequence
+        is "Geometric".
         '''
         output = ArithGeo.ArithGeo([1,2,4,8])
         self.assertEqual(output, "Geometric")
 
-    def test_NegativeAithmeticOutput(self):
+    def test_NegativeArithmeticOutput(self):
         '''
-        check if for negative arithmetic seq
-        output is "Arithmetic"
+        Checks if for negative arithmetic seq
+        output is "Arithmetic".
         '''
         output = ArithGeo.ArithGeo([-1,-2,-3,-4])
         self.assertEqual(output, "Arithmetic")
 
     def test_NegativeGeometricOutput(self):
         '''
-        check if for negative num in geometric seq
-        output is "Geometric"
+        Checks if for negative numbers in geometric sequence
+        output is "Geometric".
         '''
         output = ArithGeo.ArithGeo([-1,2,-4,8])
         self.assertEqual(output, "Geometric")
 
     def test_WrongInputSequence(self):
         '''
-        check if for wrong input seq
-        output is -1
+        Checks if for 0 in input 
+        output is -1.
         '''
-        output = ArithGeo.ArithGeo([-1,-1,0,0])
+        output = ArithGeo.ArithGeo([0,1,2,3])
         self.assertEqual(output, -1)
 
+    def test_InputContainsSameElements(self):
+        '''
+        Checks if for same elements in input 
+        output is "Input array contains same elements".
+        '''
+        output = ArithGeo.ArithGeo([1,1,1,1])
+        self.assertEqual(output, "Input array contains same elements")
+
     #endregion
+
 if __name__ == "__main__":
     '''
-    main method of unittest class
+    Main method of unittest class.
     '''
     unittest.main()
