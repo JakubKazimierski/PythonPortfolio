bd214ccfbe8d1094bbbbc7476303832c72923c2f
unknown <j.m.kazimierski@gmail.com>
2020-11-17 16:10:00 +0100

    Improved code and clarified tests code

--- CountingMinutesI/CountingMinutesI.py
+++ CountingMinutesI/CountingMinutesI.py
@@ -3,26 +3,30 @@ Counting Minutes I from coderbyte
 November 2020 Jakub Kazimierski
 '''
 
+#For matching correct input
+import re
+
 def CountingMinutesI(strParam):
     '''
-    take the str parameter being passed which will be two times
-    (each properly formatted with a colon and am or pm) 
-    separated by a hyphen and return the total number of minutes
-    between the two times
-    e.g.  if str is 9:00am-10:00am then the output should be 60
+    Have the function CountingMinutesI(str) 
+    take the str parameter being passed which 
+    will be two times (each properly formatted with a colon and am or pm) 
+    separated by a hyphen and return the total number of minutes between the two times. 
+    The time will be in a 12 hour clock format. 
+    For example: if str is 9:00am-10:00am then the output should be 60. 
+    If str is 1:00pm-11:00am the output should be 1320.
     '''
 
-    #assert inout is string and at least each hour is length 6 (H:MMnn) to 7(HH:MMnn) and also '-' sign
-    #so at least string is 6+6+1 = 13 and largest range is 7+7+1 = 15, but HH:MMnnHH:MMnn is proper length
-    #but it is wrong input so also string has to contain '-'
-    if type(strParam) == str and len(strParam)>=13 and len(strParam)<=15 and "-" in strParam:
+    #Unfortunantely long regex, asserts for correct input format. 
+    # ([from 10 to 12 or from 1 to 9 with optional leading 0]am or pm-[from 10 to 12 or from 1 to 9 with optional 0 at beginning])
+    if re.search("((1[0-2]|0?[1-9]):([0-5][0-9])([ap][m]))-((1[0-2]|0?[1-9]):([0-5][0-9])([ap][m]))", strParam) != None:
 
         
-        #split string at "-" to have range of time separated
+        #Split string at "-" to have range of time separated
         timeList = strParam.split("-")
 
         #region Variables    
-        #strings to get just HH:MM format from string
+        #Strings to get just HH:MM format from string
         startHour = ""
         startHourList = []
         startHourMinutesFormat = None
@@ -31,93 +35,85 @@ def CountingMinutesI(strParam):
         endHourList = []
         endHourMinutesFormat = None
 
-        #output values which is diff in minutes
+        #Output values which is diff in minutes
         output = None
         #endregion
 
+        #region Commentary
         #best way to count time beetween is to convert it
         #to format of 24 hours
         #what does not change in input is am or pm at two last indexes of string
         #actually we are interested just in index bfore last, those are letters 'a'  or 'p'
         #it defines hours as: ante (before) or post(after) meridiem (midday) (fun fact XD)
+        #endregion
+
+
+        #Region Change format to lists [HH,MM]
         
-        #region Change format to lists [HH,MM]
-        for i in timeList[0]:
+        #timeList will be always length of 2 thanks to input assertion
+        #timeList[0] is for startHours timeList[1] is for endHours
+        for j in range(0,2):
+            for i in timeList[j]:
 
-            #till we get first a or p letter
-            if i.isalpha() == False:
-                startHour += i
-            else:
-                #if before midday just leave at is it
-                #and split string HH:MM to list od [HH,MM]
-                if i == "a":
-                    startHourList = startHour.split(":")
-                    break
-                #if after midday add 12 to hour and ovverwrite old value in startHourList
-                #["HH+12","MM"]
-                elif i == "p":
-                    startHourList = startHour.split(":")
-                    #bufor value just for loop
-                    newFormat = int(startHourList[0]) + 12
-                    startHourList[0] = str(newFormat)
-                    break
+                #Till we get first a or p letter
+                if i.isalpha() == False:
+                    if j == 0:
+                        startHour += i
+                    else:
+                        endHour += i
                 else:
-                    break    
-        
-        #same process goes for endHour
-        for i in timeList[1]:
-
-            #till we get first a or p letter
-            if i.isalpha() == False:
-                endHour += i
-            else:
-                #if before midday just leave at is it
-                #and split string HH:MM to list od [HH,MM]
-                if i == "a":
-                    endHourList = endHour.split(":")
-                    break
-                #if after midday add 12 to hour and ovverwrite old value in endHourList
-                #["HH+12","MM"]
-                elif i == "p":
-                    endHourList = endHour.split(":")
-                    #bufor value just for loop
-                    newFormat = int(endHourList[0]) + 12
-                    endHourList[0] = str(newFormat)
-                    break
-                else:
-                    break    
-
+                    #If before midday just leave at is it
+                    #and split string HH:MM to list od [HH,MM]
+                    if i == "a":
+                        if j == 0:
+                            startHourList = startHour.split(":")
+                        else:
+                            endHourList = endHour.split(":")
+                        break
+                    #If after midday add 12 to hour and ovverwrite old value in startHourList
+                    #["HH+12","MM"]
+                    else:
+                        if j == 0:
+                            startHourList = startHour.split(":")
+                            #Bufor value just for loop
+                            newFormat = int(startHourList[0]) + 12
+                            startHourList[0] = str(newFormat)
+                        else:
+                            endHourList = endHour.split(":")
+                            newFormat = int(endHourList[0]) + 12
+                            endHourList[0] = str(newFormat)
+                        break
 
         #endregion
 
-        #start range in minutes
+        #Start range in minutes
         startHourMinutesFormat = int(startHourList[0])*60 + int(startHourList[1])
 
-        #end range in minutes
+        #End range in minutes
         endHourMinutesFormat = int(endHourList[0])*60 + int(endHourList[1])
 
-
+        #region Commentary
         #cases of output:
+        #24*60 is whole day in minutes
 
         #startHour is am and endHour is pm
-            #just abs(endHour-startHour)
+            #difference = endHour-startHour
         #startHour is am/pm and endHour is am/pm before startHour
-            # abs(24*60 - abs(endHour-startHour))
+            # difference = 24*60 - (endHour-startHour)
         #startHour is am/pm and endHour is am/pm after startHour
-            #abs(endHour-startHour)
+            # difference = endHour-startHour
         #startHour is pm and endHour is am
-            # abs(24*60 - abs(endHour-startHour))
+            # difference = 24*60 - (endHour-startHour)
 
-        #conclusion is if endHourMinutesFormat is < startHourMinutesFormat
-        # output is  abs(24*60 - abs(endHour-startHour))
-        #else abs(endHour-startHour)
-        
-        #to avoid unexpected minus in output i'll use abs
+        #conclusion is if endHourMinutesFormat is less than startHourMinutesFormat
+        # difference =  24*60 - (endHour-startHour)
+        #else deifference = (endHour-startHour)
+        #endregion
+
         if startHourMinutesFormat > endHourMinutesFormat:
-            output = abs(24*60 - abs(endHourMinutesFormat-startHourMinutesFormat))
+            output = 24*60 - (startHourMinutesFormat-endHourMinutesFormat)
         else:    
-            output = abs(endHourMinutesFormat - startHourMinutesFormat)
-
+            output = endHourMinutesFormat - startHourMinutesFormat
 
         return output
 
--- CountingMinutesI/test_CountingMinutesI.py
+++ CountingMinutesI/test_CountingMinutesI.py
@@ -8,72 +8,56 @@ import CountingMinutesI
 
 class test_CountingMinutesI(unittest.TestCase):
     '''
-    class contains unittests for CountingMinutesI.py
+    Class contains unittests for CountingMinutesI.py
     '''
 
     #region Unittests
-    def test_ExpectedOutput(self):
+    def test_StartHourIsLessThanEndHour(self):
         '''
-        check if simple input gives expected output
+        Checks if difference is correct when start hour
+        is less than end hour.
         '''
         output = CountingMinutesI.CountingMinutesI("10:00am-1:00pm")
         self.assertEqual(output, 180)
     
-    def test_FirstRangeIsLaterDiffrentFormats(self):
+    def test_StartHourIsGreater(self):
         '''
-        check if algorithm is correct
+        Checks if difference is correct
         if start hour is greater than endHour
-        and formats of hours are different e.g pm-am
+        and formats of hours are different pm-am.
         '''
         output = CountingMinutesI.CountingMinutesI("10:00pm-1:00am")
         self.assertEqual(output, 180)
     
-    def test_FirstRangeIsLaterSecondIsSameFormat(self):
+    def test_StartHourIsGreaterSameFormat(self):
         '''
-        change if alghoritm works properly
+        Checks if difference is correct
         if first hour is grater than end hour
-        and both are same format e.g pm
+        and both are same format e.g pm-pm.
         '''
         output = CountingMinutesI.CountingMinutesI("10:00pm-9:00pm")
         self.assertEqual(output, 1380)
 
     def test_Midnight(self):
         '''
-        test output for midnight hour
+        Checks if for midnight hour
         start hour is later than end hour
-        so output is whole day without oe minute
+        so output is whole day without one minute.
         '''
-    
         output = CountingMinutesI.CountingMinutesI("12:01pm-12:00pm")
         self.assertEqual(output, 1439)    
     
     def test_TooLongInput(self):
         '''
-        check if assertion catch too long input
+        Checks if assertion catch wrong input.
         '''
         output = CountingMinutesI.CountingMinutesI("10:00pm---9:00pm")
         self.assertEqual(output, "Wrong Input")
-    
-    def test_InputWithoutSign(self):
-        '''
-        check if assertion catch input without '-' sign
-        '''
-        output = CountingMinutesI.CountingMinutesI("10:00pm9:00pm")
-        self.assertEqual(output, "Wrong Input")
-
-    def test_TooShortInput(self):
-        '''
-        check if assertion catch too short input
-        '''
-        output = CountingMinutesI.CountingMinutesI("10pm-9:00pm")
-        self.assertEqual(output, "Wrong Input")
-
-
 
     #endregion
 
 if __name__ == "__main__":
     '''
-    main method for unittests
+    Main method for unittests.
     '''
     unittest.main()
