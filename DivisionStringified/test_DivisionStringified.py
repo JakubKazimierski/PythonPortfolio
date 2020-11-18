d7645289904f1e59868a6f26988b7b9cbbb8e4f4
unknown <j.m.kazimierski@gmail.com>
2020-11-18 15:11:01 +0100

    Little improvement

--- DivisionStringified/test_DivisionStringified.py
+++ DivisionStringified/test_DivisionStringified.py
@@ -8,44 +8,49 @@ import DivisionStringified
 
 class test_DivisionStringified(unittest.TestCase):
     '''
-    class contains unittests for DivisionStringified.py
+    Class contains unittests for DivisionStringified.py
     '''
 
     #region Unittests
     def test_WrongTypeInput(self):
         '''
-        checks if input is as expected
+        Checks if input has correct type.
         '''
         output = DivisionStringified.DivisionStringified(1.0,2)
-        self.assertEqual(output, "Input type has to be Intiger")
+        self.assertEqual(output, "Input type has to be Integer")
 
     def test_ComaTests(self):
         '''
-        checks if comas in digit are at correct places
+        Checks if comas in digit are at correct places.
         '''
         output = DivisionStringified.DivisionStringified(1000000,1)
         self.assertEqual(output, "1,000,000")
 
     def test_RoundingTests(self):
         '''
-        checks if function round correctly output
-        not for closest even num, but properly 
+        Checks if function round division output correctly. 
         '''
         output = DivisionStringified.DivisionStringified(9,2)
         self.assertEqual(output, "5")
 
     def test_BigNumsDivision(self):
         '''
-        checks if for big nums function work properly
+        Checks if for big nums function work properly.
         '''
         output = DivisionStringified.DivisionStringified(123456789 , 10000)
         self.assertEqual(output, "12,346")
 
+    def test_DivisionByZero(self):
+        '''
+        Checks if for num2 equal 0 assertion works properly.
+        '''
+        output = DivisionStringified.DivisionStringified(123456789 , 0)
+        self.assertEqual(output, "num2 cannot be 0")
 
     #endregion
 
 if __name__ == "__main__":
     '''
-    main method for unittests' class
+    Main method for unittests' class.
     '''
     unittest.main()
