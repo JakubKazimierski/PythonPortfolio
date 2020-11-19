05c4a2d899cc69a698557c419cd01d82496289e5
unknown <j.m.kazimierski@gmail.com>
2020-11-19 13:37:25 +0100

    improved a little bit

--- FindIntersection/FindIntersection.py
+++ FindIntersection/FindIntersection.py
@@ -5,23 +5,41 @@ october 2020 Jakub Kazimierski
 
 def FindIntersection(strArr):
   '''
-  method looks for intersection of numbers in two strings array
+  Have the function FindIntersection(strArr) 
+  read the array of strings stored in strArr which 
+  will contain 2 elements: the first element will represent 
+  a list of comma-separated numbers sorted in ascending order, 
+  the second element will represent a second list of 
+  comma-separated numbers (also sorted). 
+  Your goal is to return a comma-separated string containing 
+  the numbers that occur in elements of strArr in sorted order. 
+  If there is no intersection, return the string false.
+
+  For example: if strArr contains ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"] 
+  the output should return "1,4,13" because those numbers appear 
+  in both strings. The array given will not be empty, 
+  and each string inside the array will be of numbers 
+  sorted in ascending order and may contain negative numbers.
   '''
+  try:
 
-  #we can use built in method intersection
-  #it is method for sets
-  #so we have to make sets from our string array
+    #region Commentary
+    #We can use built in method intersection
+    #which is method for sets
+    #so we have to make sets from our string array
 
-  #split values, we dont want char "," in our intersection
-  #we are working on strings 
-  arrayX = set(strArr[0].split(", "))
-  arrayY = set(strArr[1].split(", "))
+    #Below splits values, we dont want char "," in our intersection
+    #we are working on strings 
+    #endregion
+    arrayX = set(strArr[0].split(", "))
+    arrayY = set(strArr[1].split(", "))
 
-  #sort using integer value of str as key  
-  result = sorted(list(arrayX.intersection(arrayY)), key=lambda str: int(str))
+    #Sorts using integer value of str as key  
+    result = sorted(list(arrayX.intersection(arrayY)), key=lambda str: int(str))
 
-  #join concatenate elements of set with separator  
-  return ','.join(result) if len(result) > 0 else False
-
-
+    #join() concatenate elements of set with separator  
+    return ','.join(result) if len(result) > 0 else False
+  
+  except(AttributeError, TypeError):
+    return -1
   
--- FindIntersection/test_FindIntersection.py
+++ FindIntersection/test_FindIntersection.py
@@ -1,5 +1,5 @@
 '''
-Unittests for find intersection method
+Unittests for FindIntersection.py
 October 2020 Jakub Kazimierski
 '''
 
@@ -8,51 +8,57 @@ import FindIntersection
 
 class test_FindIntersection(unittest.TestCase):
     '''
-    class with unittests for find intersection method
+    Class contains unittests for FindIntersection.py
     '''
 
     #region Unittests
     def test_SimpleIntersection(self):
         '''
-        test if output is exxpected list of numbers from both strings
+        Tests if output has expected list of numbers from both strings.
         '''
         output = FindIntersection.FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"])
         self.assertEqual(output, "1,4,13")
 
     def test_NoIntersection(self):
         '''
-        check if output is false when no intersection is found
+        Checks if output is false if no intersection was found.
         '''
         output = FindIntersection.FindIntersection(["2, 3, 4","5, 6, 7"])
         self.assertEqual(output, False)
 
     def test_RepeatedValues(self):
         '''
-        check if output is correct when values repeat few times in oth array
-        (output should be one number from both arrays if both contain it, no matter how many times 
-        it appears in both arrays)
+        Checks if output is correct if values repeat in each array
+        (output should have one number from both arrays if both contains this number, no matter how many times 
+        it appears in both arrays).
         '''
-
         output = FindIntersection.FindIntersection(["2, 3, 3","3, 3, 4, 4"])
         self.assertEqual(output, "3")
 
     def test_UnsortedArrays(self):
         '''
-        check if output is correct when arrays are not sorted
+        Checks if output is correct if arrays are not sorted.
         '''
         output = FindIntersection.FindIntersection(["4, 3, 2, 5","5, 6, 4"])
         self.assertEqual(output, "4,5")
     
     def test_UnsortedOutput(self):
         '''
-        check if output is sorted ascending
+        Checks if output is sorted ascending.
         '''
         output = FindIntersection.FindIntersection(["2, 3, 4","5, 6, 7"])
         self.assertFalse(output, "5,4")
+
+    def test_AssertInput(self):
+        '''
+        Checks if output is -1 for wrong input.
+        '''
+        output = FindIntersection.FindIntersection(["2, 3, 4",5, 6, 7])
+        self.assertTrue(output, -1)    
     #endregion
     
 if __name__ == "__main__":
     '''
-    main method for unittests
+    Main method for unittests.
     '''
     unittest.main()
