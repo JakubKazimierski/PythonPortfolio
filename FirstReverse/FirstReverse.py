a3663242c9377cd3ba7471976e8e55a1e4432993
unknown <j.m.kazimierski@gmail.com>
2020-11-22 23:15:16 +0100

    less comments

--- FirstReverse/FirstReverse.py
+++ FirstReverse/FirstReverse.py
@@ -4,23 +4,17 @@ October 2020 Jakub Kazimierski
 '''
 
 def FirstReverse(strParam):
-  '''
-  Have the function FirstReverse(str) 
-  take the str parameter being passed 
-  and return the string in reversed order. 
-  For example: if the input string is "Hello World and Coders" 
-  then your program should return the string sredoC dna dlroW olleH.
-  '''
+    '''
+    Have the function FirstReverse(str) 
+    take the str parameter being passed 
+    and return the string in reversed order. 
+    For example: if the input string is "Hello World and Coders" 
+    then your program should return the string sredoC dna dlroW olleH.
+    '''
 
-  try:
+    try:
+      # Below returns string backwards
+      return strParam[::-1]
 
-    #region Commentary
-    #the slice statement [::-1] means 
-    #start at the end of the string and 
-    #end at position 0, move with the step -1, 
-    #which means one step backwards.
-    #endregion
-    return strParam[::-1]
-
-  except TypeError:
-    return -1
+    except TypeError:
+      return -1
