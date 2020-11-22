0b93f66a10d332f2ec487127b2f89685384ab7e2
unknown <j.m.kazimierski@gmail.com>
2020-11-22 23:51:54 +0100

    code improved

--- LetterCapitalize/LetterCapitalize.py
+++ LetterCapitalize/LetterCapitalize.py
@@ -6,22 +6,14 @@ October 2020 Jakub Kazimierski
 import string
 
 def LetterCapitalize(strParam):
-  '''
-  Have the function LetterCapitalize(str) 
-  take the str parameter being passed and 
-  capitalize the first letter of each word. 
-  Words will be separated by only one space.
-  '''
+   '''
+   Have the function LetterCapitalize(str) 
+   take the str parameter being passed and 
+   capitalize the first letter of each word. 
+   Words will be separated by only one space.
+   '''
 
-  #Below makes list of words by splitting  given string at spaces
-  wordsList = strParam.split(" ")
+   wordsList = strParam.split(" ")
 
-  outputList = []
-
-  #Below loop iterates words list and capitalize each one
-  for i in wordsList:
-     outputList.append(i.capitalize())
-    
-
-  #Joins capitalized words in list by spaces
-  return " ".join(outputList)
+   # return capitalized words joined by spaces         
+   return " ".join((word.capitalize() for word in wordsList))
