98ab9d780588fada48bea4bc7e71e72341a6fe97
unknown <j.m.kazimierski@gmail.com>
2020-11-23 12:30:38 +0100

    cleared code

--- MeanMode/MeanModeFaster.py
+++ MeanMode/MeanModeFaster.py
@@ -10,16 +10,20 @@ import statistics
 
 def MeanModeFaster(arr):
     '''
-    return 1 if the mode equals the mean
-    0 if they don't equal each other
-    The input array will not be empty
-    will only contain positive integers
+    Have the function MeanMode(arr) 
+    take the array of numbers stored in arr 
+    and return 1 if the mode equals the mean, 
+    0 if they don't equal each other 
+    (ie. [5, 3, 3, 3, 1] should return 1 
+    because the mode (3) equals the mean (3)). 
+    The array will not be empty, 
+    will only contain positive integers, 
     and will not contain more than one mode.
     '''
   
     try:
-        #collection statistics has built in
-        #functions mean and mode
+        # built in mode raises StatisticsError if there is more than one mode
+        # (use counter mostcommon if there is more than one mode)
         mean = statistics.mean(arr)
         mode = statistics.mode(arr)
 
