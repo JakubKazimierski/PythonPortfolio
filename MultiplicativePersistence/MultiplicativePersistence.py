ba46d073f7219df10180af845a80bac14b1ab381
unknown <j.m.kazimierski@gmail.com>
2020-11-23 22:54:39 +0100

    Improved code

--- MultiplicativePersistence/MultiplicativePersistence.py
+++ MultiplicativePersistence/MultiplicativePersistence.py
@@ -3,38 +3,45 @@ MultiplicativePersistence from Coderbyte
 November 2020 Jakub Kazimierski
 '''
 
-#import math collections to use built in multiplication method
+# math collections to use prod()
 import math 
 
 def MultiplicativePersistence(num):
     '''
-    take the num parameter being passed 
-    which will always be a positive integer
-    and return its multiplicative persistence which is the number of times
-    you must multiply the digits in num until you reach a single digit
+    Have the function MultiplicativePersistence(num) 
+    take the num parameter being passed which will always 
+    be a positive integer and return its multiplicative persistence 
+    which is the number of times you must multiply the digits in num until 
+    you reach a single digit. For example: if num is 39 then your program 
+    should return 3 because 3 * 9 = 27 then 2 * 7 = 14 
+    and finally 1 * 4 = 4 and you stop at 4.
     '''
 
-    if type(num) == int and num >= 0:
-        #counter
+    try:
+        
         counterOfSteps = 0
-        #assign value of input to variable to work with
-        buforNum = num
+        
+        # as long as there is more than one digit
+        while len(str(num)) > 1:
 
-
-        #till we get one digit as output of multiplication
-        #convert bufor to string to take length of it
-        while len(str(buforNum)) > 1:
-
-            #create list of digits
-            #convert type int to string to get each elem 
-            listOfDIgits = [int(i) for i in str(buforNum)]
-
-            #assign new value after multiplication of elements to buforNum
-            buforNum = math.prod(listOfDIgits)
+            # reassign num as product of it's digits
+            num = math.prod([int(i) for i in str(num)])
 
             counterOfSteps += 1
 
         return counterOfSteps
 
-    else:
+    except ValueError:
         return -1    
+
+def _input():
+
+    exampleInput = 39
+
+    return exampleInput
+
+def _output():
+
+    exampleOutput = 3
+
+    return exampleOutput
--- MultiplicativePersistence/test_MultiplicativePersistence.py
+++ MultiplicativePersistence/test_MultiplicativePersistence.py
@@ -9,14 +9,14 @@ import MultiplicativePersistence
 
 class test_MultiplicativePersistence(unittest.TestCase):
     '''
-    class contains unittests for MultiplicativePersistence.py
+    Class contains unittests for MultiplicativePersistence.py
     '''
 
     #region Unittests
     def test_ExpectedOutput(self):
         '''
-        test if output is proper
-        e.g for 39 it should be 3
+        Checks if output is proper
+        e.g for 39 it should be 3.
         '''
         output = MultiplicativePersistence.MultiplicativePersistence(39)
         self.assertEqual(output, 3)
@@ -24,33 +24,22 @@ class test_MultiplicativePersistence(unittest.TestCase):
 
     def test_OneDigitInput(self):
         '''
-        check if for one digit in input
-        output is 0
+        Checks if for one digit in input output is 0.
         '''
         output = MultiplicativePersistence.MultiplicativePersistence(9)
         self.assertEqual(output, 0)
 
     def test_WrongInputType(self):
         '''
-        check if for wrong input type
-        output is -1
+        Checks if for wrong input type output is -1.
         '''
-        output = MultiplicativePersistence.MultiplicativePersistence("9")
-        self.assertEqual(output, -1)
-
-
-    def test_NegativeIntigerInput(self):
-        '''
-        check if for negative inut intiger
-        output is -1
-        '''
-        output = MultiplicativePersistence.MultiplicativePersistence(-9)
+        output = MultiplicativePersistence.MultiplicativePersistence("ad")
         self.assertEqual(output, -1)
 
     #endregion
 
 if __name__ == "__main__":
     '''
-    main method of unittests
+    Main method of unittests.
     '''
     unittest.main()
