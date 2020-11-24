f8d94da24dcd205f4c823402c9f7dbea14c133a5
unknown <j.m.kazimierski@gmail.com>
2020-11-24 21:53:39 +0100

    Full task description

--- Easy/NumberAddition/NumberAddition.py
+++ Easy/NumberAddition/NumberAddition.py
@@ -5,9 +5,17 @@ November 2020 Jakub Kazimierski
 
 def NumberAddition(strParam):
     '''
-    take the str parameter, search for all the numbers in the string
-    add them together,  You will have to differentiate between single digit numbers
-    and multiple digit numbers. Each input string will contain at least one letter or symbol.
+    Have the function NumberSearch(str) 
+    take the str parameter, search for all 
+    the numbers in the string, add them together, 
+    then return that final number. 
+    For example: if str is "88Hello 3World!" 
+    the output should be 91. 
+    
+    You will have to differentiate between single digit numbers 
+    and multiple digit numbers like in the example above. 
+    So "55Hello" and "5Hello 5" should return two different answers. 
+    Each string will contain at least one letter or symbol.
     '''
 
     try:
