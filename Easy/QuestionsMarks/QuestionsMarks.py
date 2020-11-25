92af939de7c7f79e8a83b5a40bf7b8700034f9f5
unknown <j.m.kazimierski@gmail.com>
2020-11-25 23:40:06 +0100

    improved code

--- Easy/QuestionsMarks/QuestionsMarks.py
+++ Easy/QuestionsMarks/QuestionsMarks.py
@@ -1,5 +1,5 @@
 '''
-QuestionMarks from codersbyte
+QuestionMarks from Coderbyte
 October 2020 Jakub Kazimierski
 '''
 
@@ -7,79 +7,69 @@ from collections import Counter
 
 
 def QuestionsMarks(strParam):
-  '''
-  method return true if
-  there are exactly 3 question marks
-  between every pair of two numbers that add up to 10
-  '''
-
-  #make counter which contains
-  #number of questionmarks
-  #in renge beetween numbers that
-  #add up to 10
-  #frequencyQuestionMark = Counter(strParam)
-  output = []
-
-  #var to loop while length of given input string
-  i = 0
-  
-  #start traverse
-  while i < len(strParam):  #O(n)
+    '''
+    Have the function QuestionsMarks(str) 
+    take the str string parameter, 
+    which will contain single digit numbers, 
+    letters, and question marks, and check 
+    if there are exactly 3 question marks between 
+    every pair of two numbers that add up to 10. 
     
-    #if substring represent digit value
-    if strParam[i].isdigit() == True:
-      
-      #find next digit value in range form i to next value
-      for j in range(i, len(strParam)): #O(n)
+    If so, then your program should return the string true,
+    otherwise it should return the string false. 
+    If there aren't any two numbers that add up to 10 in 
+    the string, then your program should return false as well.
 
-        #if we find it
-        if strParam[j].isdigit() == True:
+    For example: if str is "arrb6???4xxbl5???eee5" 
+    then your program should return true because there 
+    are exactly 3 question marks between 6 and 4, 
+    and 3 question marks between 5 and 5 at the end of the string.
+    '''
 
-          #if sum of digit values add up to 10
-          if int(strParam[i]) + int(strParam[j]) == 10 and i < j:
-          
-            #count question marks between
-            frequencyQuestionMark = Counter(strParam[i:j])
-            #return true when there are 3 question marks
-          
-            if frequencyQuestionMark["?"] == 3:
-          
-              output.append("true")
-              #reset counter
-              frequencyQuestionMark.clear()
-              #continue searching next digits
-              i = j
-          
-            else:
-          
-              output.append("false")
-              #reset counter
-              frequencyQuestionMark.clear()
-              i = j                
-          
-          #if sum is not valid i became value at j index in strParam
-          #and whole process continue    
-          else:
-            i = j
-            continue
+    try:
+        
+        output = "true"
+        previousDigit_remember = 0
+        questionsMarks_counter = 0
+        sum_to_ten_counter = 0
+
+        for i in strParam:
+            
+            if i.isdigit():
+                
+                if int(i) + previousDigit_remember == 10:
+                    
+                    sum_to_ten_counter += 1
+                    
+                    if questionsMarks_counter != 3:
+                        output = "false"
+                
+                # reset question_marks counter  
+                questionsMarks_counter = 0    
+            
+                # remember previous digit
+                previousDigit_remember = int(i)
+            
+            elif i == "?":
+                questionsMarks_counter += 1
+
+        if sum_to_ten_counter == 0:
+          output = "false"  
+
+
+        return output
+
+    except(AttributeError, TypeError, ValueError):
+       return -1
+
+def _input():
     
-    #if no of above conditions were true, just traverse further      
-    i+=1
+    exampleInput = "arrb6???4xxbl5???eee5" 
 
-  #I assume that rhanks to 2 times loop complexity is O(n^2)
+    return exampleInput
 
-  #if input is correct return true or false
-  if strParam != "" and type(strParam) == str:
+def _input():
+    
+    exampleOutput = "true"
 
-    #if all digits that add up to 10 has 3 '?' between 
-    #in output are only true values
-    if "false" not in output and output != []:
-
-      return "true"
-    else:  
-
-      return "false"
-  else:
-
-    #if input was not correct
-    return -1
+    return exampleOutput
--- Easy/QuestionsMarks/test_QuestionsMarks.py
+++ Easy/QuestionsMarks/test_QuestionsMarks.py
@@ -1,5 +1,5 @@
 '''
-TestsUnits fro QuestionsMarks.py from codersbyte
+Unittests for QuestionsMarks.py
 October/November 2020 Jakub Kazimierski
 '''
 
@@ -9,53 +9,58 @@ import QuestionsMarks
 
 class test_QuestionsMarks(unittest.TestCase):
     '''
-    class contains unittests for QuestionsMaks.py from codersbyte
+    Class contains unittests for QuestionsMaks.py
     '''
 
     #region Unittests
     def test_trueOutput(self):
         '''
-        check ifoutput is true as expected
+        Checks if output is true for proper input. 
         '''
         output = QuestionsMarks.QuestionsMarks("5??aaaaaaaaaaaaaaaaaaa?5?a??5")
         self.assertEqual(output, "true")
 
     def test_notEnoughQuestionsMarksOutput(self):
         '''
-        check if output is false when there is not enough
-        questions marks
+        Checks if output is false when there is not enough
+        questions marks.
         '''
         output = QuestionsMarks.QuestionsMarks("arrb6??4xxbl5??eee5")
         self.assertEqual(output, "false")
 
 
-    def test_EnoughQuestionsMarksDigitsNotAddUpToTen(self):
+    def test_DigitsNotAddUpToTen(self):
         '''
-        check if output is false 
-        when numbers not add up to 10
+        Checks if output is false 
+        when numbers not add up to 10.
         '''
         output = QuestionsMarks.QuestionsMarks("arrb6???xxbl5???eee4")
         self.assertEqual(output, "false")
 
     def test_NoDigits(self):
         '''
-        check if output is false when there is no digits
+        Checks if output is false when there is no digits.
         '''
         output = QuestionsMarks.QuestionsMarks("arrb???xxbl???eee")
         self.assertEqual(output, "false")
 
-    def test_NotValidInput(self):
+    def test_EmptyInput(self):
         '''
-        check if output is -1 when
-        input is empty
+        Checks if output is false when input is empty.
         '''
         output = QuestionsMarks.QuestionsMarks("")
-        self.assertEqual(output, -1)
+        self.assertEqual(output, "false")
 
+    def test_NotValidInput(self):
+        '''
+        Checks if output is -1 for wrong type input.
+        '''
+        output = QuestionsMarks.QuestionsMarks(123)
+        self.assertEqual(output, -1)
     #endregion
     
 if __name__ == "__main__":
     '''
-    main method for unittests class
+    Main method for unittests class
     '''
     unittest.main()        
