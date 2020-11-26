5ff80f6e0086ed11e2488216d2e8363c152ce4a0
unknown <j.m.kazimierski@gmail.com>
2020-11-26 16:50:14 +0100

    improved code

--- Easy/SimpleSymbols/SimpleSymbols.py
+++ Easy/SimpleSymbols/SimpleSymbols.py
@@ -3,41 +3,49 @@ SimpleSymbols from Codersbyte
 October 2020 Jakub Kazimierski
 '''
 
+import re
+
 def SimpleSymbols(strParam):
-  '''
-  method check if each letter in given
-  input string is surrounded by '+' character
-  '''
-
-  #counter of letters for check if input is correct
-  counterLetters = 0
-
-  for i in range(0, len(strParam)):
-    #if letter is at index 0 false
-    if strParam[i].isalpha() and i == 0:
-      return "false"
-    #if letter is at last index false  
-    if strParam[i].isalpha() and i == len(strParam)-1:
-      return "false"
+    '''
+    Have the function SimpleSymbols(str) 
+    take the str parameter being passed 
+    and determine if it is an acceptable 
+    sequence by either returning the string 
+    true or false. 
     
-    if i > 0 and i < len(strParam)-1:  
-    #if letter is not neighbour with + at left false  
-     if strParam[i].isalpha() and strParam[i-1] != "+":
+    The str parameter will be composed of + and = 
+    symbols with several characters between them 
+    (ie. ++d+===+c++==a) and for the string to be 
+    true each letter must be surrounded by a "+" symbol.
+    So the string to the left would be false. 
+    The string will not be empty and will have at least one letter.
+    '''
+
+    #  is first or last element is letter output cannot be true
+    if strParam[-1].isalpha() or strParam[0].isalpha():
         return "false"
-    #if letter is not neighbour with + at right false    
-     if strParam[i].isalpha() and strParam[i+1] != "+":
-        return "false"
 
-    #above cheks not correct cases
 
-    if strParam[i].isalpha():
-      counterLetters += 1
+    # remove "+" from string and make list of splitted characters
+    listWithout_plus = strParam.split("+")
 
-  #if there is no letters false
-  if counterLetters == 0:
-    return "false"
-  else:  
+
+    for i in listWithout_plus:
+        # if element of list is not letter it is allowed to contain only "=" characters
+        if i.isalpha() != True:
+          if re.search("[a-zA-z]",i) != None:
+              return "false"      
+
     return "true"
+  
+def _input():
 
-# keep this function call here 
-#print(SimpleSymbols(input()))
+    exampleInput = "++d+===+c++==a"
+
+    return exampleInput
+
+def _output():
+
+    exampleOutput = "false"
+
+    return exampleOutput
--- Easy/SimpleSymbols/test_SimpleSymbols.py
+++ Easy/SimpleSymbols/test_SimpleSymbols.py
@@ -9,38 +9,34 @@ import SimpleSymbols
 
 class test_SimpleSymbols(unittest.TestCase):
     '''
-    class with unittests for SimpleSymbols.py
+    Class contains unittests for SimpleSymbols.py
     '''
 
     #region Unittests
     def test_ProperOutput(self):
         '''
-        checks if output is true
-        when each letter is surrounded by '+' char
+        Checks if output is true when each letter is surrounded by '+' char.
         '''
         output = SimpleSymbols.SimpleSymbols("+t+==+r+r+")
         self.assertEqual(output, "true")
 
     def test_LetterAtFirstIndex(self):
         '''
-        checks  if output is false
-        when at first index is letter
+        Checks  if output is false if letter is at first index.
         '''
         output = SimpleSymbols.SimpleSymbols("t+==+r+r+")
         self.assertEqual(output, "false")
 
     def test_LetterAtLastIndex(self):
         '''
-        check if output is false 
-        when letter is at last index
+        Checks  if output is false if letter is at last index.
         '''
         output = SimpleSymbols.SimpleSymbols("+==+r+r+t")
         self.assertEqual(output, "false")
 
     def test_LetterNotSurrounded(self):
         '''
-        check if output is false
-        when letter is not surrounded by '+' char
+        Checks if output is false if letter is not surrounded by '+' char.
         '''
         output = SimpleSymbols.SimpleSymbols("+==+r+r+t=")
         self.assertEqual(output, "false")
@@ -48,6 +44,6 @@ class test_SimpleSymbols(unittest.TestCase):
 
 if __name__ == "__main__":
     '''
-    main method for unittests
+    Main method for unittests.
     '''
     unittest.main()
