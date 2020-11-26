4d25b935600fb8713b508aa2483dda68251c2ea5
unknown <j.m.kazimierski@gmail.com>
2020-11-26 16:23:03 +0100

    corrected code

--- Easy/SimpleAdding/SImpleAdding.py
+++ Easy/SimpleAdding/SImpleAdding.py
@@ -1,22 +1,27 @@
 '''
-SimpleAdding from codersbyte
+SimpleAdding from Coderbyte
 October 2020 Jakub Kazimierski
 '''
 
 def SimpleAdding(num):
   '''
-  function SimpleAdding(num) add up
-  all the numbers from 1 to num.
+  Have the function SimpleAdding(num) 
+  add up all the numbers from 1 to num. 
+  
+  For example: if the input is 4 then 
+  your program should return 10 because 
+  1 + 2 + 3 + 4 = 10. 
+  For the test cases, the parameter num will 
+  be any number from 1 to 1000.
   '''
 
-  if type(num) == int:
-    #to make program simple
-    #i reccomend to use formula for arythmetic sum
+  try:
 
+    # below uses formula for arythmetic sum
     output = int(((num + 1)*num)/2)
     return output
   
-  else:
+  except(ValueError, TypeError):
     return -1
   
 
--- Easy/SimpleAdding/test_SImpleAdding.py
+++ Easy/SimpleAdding/test_SImpleAdding.py
@@ -8,22 +8,20 @@ import SImpleAdding
 
 class test_SImpleAdding(unittest.TestCase):
     '''
-    class with unittests
-    for SimpleAdding
+    Class contains unittests for SimpleAdding.py
     '''
 
     #region Unittests
     def test_ProperOutput(self):
         '''
-        checks if output is as expected
+        Checks if output is proper sum from 1 to num.
         '''
         output = SImpleAdding.SimpleAdding(12)
         self.assertEqual(output, 78)
 
     def test_NotProperInput(self):
         '''
-        checks if output is -1
-        when input is wrong type
+        Checks if output is -1 when type of input is wrong.
         '''
         output = SImpleAdding.SimpleAdding("12")
         self.assertEqual(output, -1)
@@ -31,6 +29,6 @@ class test_SImpleAdding(unittest.TestCase):
 
 if __name__ == "__main__":
     '''
-    main method for unittests class
+    Main method for unittests class.
     '''
     unittest.main()
