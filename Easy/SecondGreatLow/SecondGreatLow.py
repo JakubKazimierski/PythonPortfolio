272c92659c328a96aaecec8e969db8f5281dc59a
unknown <j.m.kazimierski@gmail.com>
2020-11-26 08:47:15 +0100

    improved code

--- Easy/SecondGreatLow/SecondGreatLow.py
+++ Easy/SecondGreatLow/SecondGreatLow.py
@@ -5,55 +5,51 @@ November 2020 Jakub Kazimierski
 
 def SecondGreatLow(arr):
     '''
-    return the second lowest and second greatest numbers, respectively
+    Have the function SecondGreatLow(arr) 
+    take the array of numbers stored in arr 
+    and return the second lowest and second greatest 
+    numbers, respectively, separated by a space. 
+    
+    For example: if arr contains [7, 7, 12, 98, 106] 
+    the output should be "12 98". 
+    The array will not be empty and will contain at least 2 numbers. 
+    It can get tricky if there's just two numbers!
     '''
     
     try:
-        #default values for output
-        secondMin = None
-        secondMax = None
 
-        #range values to work with
-        secondMinBufor = min(arr)
-        secondMaxBufor = max(arr)
+        # make set of uniqe values
+        setOfNumbers = set(arr)
 
-        #if array has length > 2 we can use algo under
-        if len(arr) > 2:
-            #check all values in arr range values
-            while secondMinBufor < max(arr):
-
-                #iterate from min to max by 1 step
-                secondMinBufor += 1
-                
-                #if at some of steps value is in arr we have looked for value
-                if secondMinBufor in arr:
-                    secondMin = secondMinBufor
-                    #loop has to end working aat this moment
-                    break
-                else:
-                    continue    
-
-            while secondMaxBufor > min(arr):
-
-                #iterate from min to max by 1 step
-                secondMaxBufor -= 1
-                
-                #if at some of steps value is in arr we have looked for value
-                if secondMaxBufor in arr:
-                    secondMax = secondMaxBufor
-                    break
-                else:
-                    continue    
-
-        #if array contain just 2 elem    
+        # if there are just 2 uniqe values max is second min, and min is second max
+        if len(setOfNumbers) < 3:
+            secondMinValue = max(setOfNumbers)
+            secondMaxValue = min(setOfNumbers)
+            return " ".join([str(secondMinValue), str(secondMaxValue)])
         else:
-            secondMin = arr[0]
-            secondMax = arr[1]
+            # for set containing more than 2 elements pop max and pop min
+            # and assign min and max values (if it is the same number it also fullfills task condition)
+            sortedSet = sorted(setOfNumbers)
+            sortedSet.pop(0)
+            sortedSet.pop()
+
+            secondMinValue = min(sortedSet)
+            secondMaxValue = max(sortedSet)
+
+            return " ".join([str(secondMinValue), str(secondMaxValue)])
 
 
-        return  str(secondMin) + " " + str(secondMax)
-
-    except Exception:
+    except (AttributeError, TypeError, ValueError):
         return -1
 
-    
+def _input():
+
+    exampleInput = [7, 7, 12, 98, 106] 
+
+    return exampleInput
+
+def _output():
+
+    exampleOutput = "12 98"
+
+    return exampleOutput    
--- Easy/SecondGreatLow/test_SecondGreatLow.py
+++ Easy/SecondGreatLow/test_SecondGreatLow.py
@@ -8,47 +8,42 @@ import SecondGreatLow
 
 class test_SecondGreatLow(unittest.TestCase):
     '''
-    class contains unittests
-    for SecondGreatLow.py
+    Class contains unittests for SecondGreatLow.py
     '''
 
     #region Unittests
     def test_ProperOutput(self):
         '''
-        check if output is as expected
+        Checks if output is as expected.
         '''
         output = SecondGreatLow.SecondGreatLow([1, 42, 42, 180])
         self.assertEqual(output, "42 42")
 
     def test_TwoValuesArr(self):
         '''
-        check if algorithm works
-        properly if array is length 2
+        Checks if algorithm works properly if array is lenght of 2.
         '''
         output = SecondGreatLow.SecondGreatLow([1, 180])
-        self.assertEqual(output, "1 180")
+        self.assertEqual(output, "180 1")
 
     def test_NegativeValuesArr(self):
         '''
-        check if algorithm works
-        properly with negative numbers
+        Checks if algorithm works properly with negative numbers.
         '''
         output = SecondGreatLow.SecondGreatLow([1, -43, -2, 180])
         self.assertEqual(output, "-2 1")
 
-
     def test_WrongInput(self):
         '''
-        check if for wrong input
-        output is -1
+        Cheks if output is equal -1 for wrong input type.
         '''
-        output = SecondGreatLow.SecondGreatLow([1])
+        output = SecondGreatLow.SecondGreatLow(["1", "-43", -2, 180])
         self.assertEqual(output, -1)
+
     #endregion
 
 if __name__ == "__main__":
     '''
-    main method of unittests
-    class
+    Main method of unittests.
     '''
     unittest.main()
