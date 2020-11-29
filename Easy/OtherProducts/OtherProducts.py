fa0e46fbb8d4dcaf58b8ee503bd437da32c91b8f
unknown <j.m.kazimierski@gmail.com>
2020-11-29 20:47:56 +0100

    Next challenge

---
+++ Easy/OtherProducts/OtherProducts.py
@@ -0,0 +1,46 @@
+'''
+Other Products from Coderbyte
+November 2020 Jakub Kazimierski
+'''
+
+import math
+
+def OtherProducts(arr):
+    '''
+    Have the function OtherProducts(arr) 
+    take the array of numbers stored in arr 
+    and return a new list of the products of 
+    all the other numbers in the array for each element. 
+    
+    For example: if arr is [1, 2, 3, 4, 5] then 
+    the new array, where each location in the new 
+    array is the product of all other elements, 
+    is [120, 60, 40, 30, 24]. 
+    
+    The following calculations were performed to get this answer: 
+    [(2*3*4*5), (1*3*4*5), (1*2*4*5), (1*2*3*5), (1*2*3*4)].
+    You should generate this new array and then return the numbers 
+    as a string joined by a hyphen: 120-60-40-30-24. 
+    The array will contain at most 10 elements and at least 1 element 
+    of only positive integers.
+    '''
+
+    try:
+
+        # we can divide by i, because array contains only positive integers
+        return "-".join(str(int(math.prod(arr)/i)) for i in arr)
+  
+    except(AttributeError, TypeError):
+        return -1
+        
+def _input():
+
+    exampleInput = [1, 2, 3, 4, 5]
+
+    return exampleInput
+
+def _output():
+
+    exampleOutput = "120-60-40-30-24"
+
+    return exampleOutput
---
+++ Easy/OtherProducts/test_OtherProducts.py
@@ -0,0 +1,35 @@
+'''
+Unittests for OtherProducts.py
+November 2020 Jakub Kazimierski
+'''
+
+import unittest
+import OtherProducts
+
+class test_OtherProducts(unittest.TestCase):
+    '''
+    Class contains unittests for OtherProducts.py
+    '''
+
+    # region Unittests
+    def test_ExpectedOutput(self):
+        '''
+        Cheks if output is as expected.
+        '''
+        output = OtherProducts.OtherProducts([1,2,3,4,5])
+        self.assertEquals(output, "120-60-40-30-24")
+
+    def test_WrongInput(self):
+        '''
+        Cheks if output is equal -1 for worng input type.
+        '''
+        output = OtherProducts.OtherProducts(["1",2,3,4,5])
+        self.assertEquals(output, -1)
+
+    # endregion
+
+if __name__ == "__main__":
+    '''
+    Main method for unittests.
+    '''
+    unittest.main()
