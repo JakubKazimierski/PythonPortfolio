fd7dab28de6631c345822e1d29108716b57af1ea
unknown <j.m.kazimierski@gmail.com>
2020-11-29 15:43:46 +0100

    base

---
+++ Easy/BitwiseOne/BitwiseOne.py
@@ -0,0 +1,19 @@
+'''
+Bitwise One from Coderbyte
+November 2020 Jakub Kazimierski
+'''
+
+
+
+def _input():
+
+    exampleInput = ""
+
+    return exampleInput
+
+def _output():
+
+    exampleOutput = ""
+
+    return exampleOutput
+    
---
+++ Easy/BitwiseOne/test_BitwiseOne.py
@@ -0,0 +1,24 @@
+'''
+Unittests for BitwiseOne.py
+November 2020 Jakub Kazimierski
+'''
+
+import unittest
+import BitwiseOne
+
+class test_BitwiseOne(unittest.TestCase):
+    '''
+    Class contains unittests for BitwiseOne.py
+    '''
+
+    # region Unittests
+    def test_ExpectedOutput(self):
+        pass
+
+    # endregion
+
+if __name__ == "__main__":
+    '''
+    Main method for unittests.
+    '''
+    unittest.main()
