db4ffdecc15efcdecc2374ab312ede7fee8397b8
unknown <j.m.kazimierski@gmail.com>
2020-11-30 11:29:09 +0100

    improved

--- Easy/WaveSorting/test_WaveSorting.py
+++ Easy/WaveSorting/test_WaveSorting.py
@@ -19,13 +19,6 @@ class test_WaveSorting(unittest.TestCase):
         output = WaveSorting.WaveSorting([0, 4, 22, 4, 14, 4, 2])
         self.assertEqual(output, "true")
 
-    def test_WrongInput(self):
-        '''
-        Cheks if output is equal -1 for worng input type.
-        '''
-        output = WaveSorting.WaveSorting([0, "4", 22, 4, 14, 4, 2])
-        self.assertEqual(output, -1)
-
     # endregion
 
 if __name__ == "__main__":
