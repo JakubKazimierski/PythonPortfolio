4ff6ccbe4ad90fe40f9c83beb0cef8fac0bb5ae8
unknown <j.m.kazimierski@gmail.com>
2020-12-02 13:23:36 +0100

    Next Challenge

---
+++ Easy/PowerSetCount/PowerSetCount.py
@@ -0,0 +1,53 @@
+'''
+PowerSetCount from Coderbyte
+December 2020 Jakub Kazimierski
+'''
+
+from itertools import chain,combinations
+
+def PowerSetCount(arr):
+    '''
+    Have the function PowerSetCount(arr) 
+    take the array of integers stored in arr, 
+    and return the length of the power set 
+    (the number of all possible sets) 
+    that can be generated. 
+    
+    For example: if arr is [1, 2, 3], 
+    then the following sets form the power set:
+
+    []
+    [1]
+    [2]
+    [3]
+    [1, 2]
+    [1, 3]
+    [2, 3]
+    [1, 2, 3]
+
+    You can see above all possible sets, 
+    along with the empty set, are generated. 
+    Therefore, for this input, your program should return 8.
+    '''
+    
+    try:
+
+        # below chains all combinations into one iterable, then converts object into list
+        power_set = list(chain(*map(lambda x: combinations(arr, x), range(0, len(arr)+1))))
+
+        return len(power_set)
+
+    except(AttributeError, TypeError):
+        return -1
+
+def _input():
+
+    sampleList = ["10111", "01101"]
+
+    return sampleList
+
+def _output():
+
+    sampleString = "00101"
+
+    return sampleString
---
+++ Easy/PowerSetCount/test_PowerSetCount.py
@@ -0,0 +1,37 @@
+'''
+Unittests for PowerSetCount.py
+December 2020 Jakub Kazimierski
+'''
+
+import unittest
+import PowerSetCount
+
+class test_PowerSetCount(unittest.TestCase):
+    '''
+    Class contains unittests for
+    PowerSetCount.py
+    '''
+
+    #region Unittests
+    def test_ExpectedOutput(self):
+        '''
+        Checks is output is as expected.
+        '''
+        output = PowerSetCount.PowerSetCount([1, 2, 3])
+        self.assertEqual(output, 8)
+
+    def test_WrongInput(self):
+        '''
+        Checks is output is equal -1 if input is wrong.
+        '''
+        pass
+        output = PowerSetCount.PowerSetCount(10111)
+        self.assertEqual(output, -1)
+
+    #endregion
+
+if __name__ == "__main__":
+    '''
+    Main method of unittest class.
+    '''
+    unittest.main()
