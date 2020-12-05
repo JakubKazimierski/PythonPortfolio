031c41b3f87c32f6432d6ae0ee27decbfa05525f
unknown <j.m.kazimierski@gmail.com>
2020-12-05 15:26:26 +0100

    Next challenge

---
+++ Easy/ThreeNumbers/ThreeNumbers.py
@@ -0,0 +1,52 @@
+'''
+Three Numbers from Coderbyte
+December 2020 Jakub Kazimierski
+'''
+
+import re
+
+def ThreeNumbers(strParam):
+    '''
+    Have the function ThreeNumbers(str) 
+    take the str parameter being passed 
+    and determine if exactly three unique, 
+    single-digit integers occur within each 
+    word in the string. 
+    
+    The integers can appear anywhere in the word, 
+    but they cannot be all adjacent to each other. 
+    If every word contains exactly 3 unique integers 
+    somewhere within it, then return the string true, 
+    
+    otherwise return the string false. For example: if str is 
+    "2hell6o3 wor6l7d2" then your program should return "true" 
+    but if the string is "hell268o w6or2l4d" then your program 
+    should return "false" because all the integers are adjacent 
+    to each other in the first word.
+    '''
+    
+    try:
+        # split numeric characters
+        only_numbers = list(filter(None, re.split(r"[a-zA-z ]", strParam)))
+
+        # if any splitted character has lenght > 1 return false
+        for number in only_numbers:
+            if len(number) > 1:
+                return "false" 
+        
+        return "true"
+    except(AttributeError, TypeError):
+        return -1
+        
+
+def _input():
+
+    sampleList = ["[5, 9]", "[1, 2, 6, 7]"]
+
+    return sampleList
+
+def _output():
+
+    sampleString = "2,6"
+
+    return sampleString
---
+++ Easy/ThreeNumbers/test_ThreeNumbers.py
@@ -0,0 +1,44 @@
+'''
+Unittests for ThreeNumbers.py
+December 2020 Jakub Kazimierski
+'''
+
+import unittest
+import ThreeNumbers
+
+class test_ThreeNumbers(unittest.TestCase):
+    '''
+    Class contains unittests for
+    ThreeNumbers.py
+    '''
+
+    #region Unittests
+    def test_ExpectedOutput(self):
+        '''
+        Checks if output is as expected.
+        '''
+        output = ThreeNumbers.ThreeNumbers("2hell6o3 wor6l7d2")
+        self.assertEqual(output, "true")
+
+    def test_FalseOutput(self):
+        '''
+        Checks if output is equal false for adjacent digits in input.
+        '''
+        output = ThreeNumbers.ThreeNumbers("2hell6o3 wor617d2")
+        self.assertEqual(output, "false")
+
+    def test_WrongInput(self):
+        '''
+        Checks if output is equal -1 for wrong input.
+        '''
+        output = ThreeNumbers.ThreeNumbers(12)
+        self.assertEqual(output, -1)
+
+
+    #endregion
+
+if __name__ == "__main__":
+    '''
+    Main method of unittest class.
+    '''
+    unittest.main()
