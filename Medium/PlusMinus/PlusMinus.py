748f8b3c10eb0f143eabc8f7a968805c7af2a189
JakubKazimierski <j.m.kazimierski@gmail.com>
2020-12-31 11:22:06 +0100

    Improved

--- Medium/PlusMinus/PlusMinus.py
+++ Medium/PlusMinus/PlusMinus.py
@@ -2,7 +2,7 @@
 Plus Minus from Coderbyte
 December 2020 Jakub Kazimierski
 '''
-
+import itertools
 from itertools import permutations
 from itertools import zip_longest
 
@@ -30,45 +30,14 @@ def PlusMinus(num):
     For example: if num is 26712 your program should return -+-- 
     and not +-+-.
     '''
+
+    num_str = str(num); N_oper = len(num_str) - 1
+    if not N_oper: return 'not possible'
+    # all possible combinations as binary representation of each num to 2^n
+    for n in range(2**N_oper):
+        combo = f'{n:0{N_oper}b}'.replace('0', '-').replace('1', '+') + ' '
+        # returns first occurence, because it's starting with '-' combinations
+        if not eval(''.join(''.join(pair) for pair in zip(num_str, combo))): return combo[:-1]
+    return 'not possible'
     
-    separated_digits = list(str(num))
-    
-    signs = []
-    for _ in range(len(separated_digits)):
-        signs.append("-")
-    for _ in range(len(separated_digits)):        
-        signs.append("+")
-    
-    sign_combinations = set(list(permutations(signs, len(separated_digits)-1)))
-
-    possible_signs_order = []
-    for signs in sign_combinations:
-        output = ""
-        for digit, sign in zip_longest(separated_digits, signs):
-            if sign != None:
-                output += digit + sign
-            else:
-                output += digit
-
-
-        if eval(output) == 0:
-            possible_signs_order.append(signs)
-
-    if len(possible_signs_order) == 0:
-        return "not possible"
-    
-    
-    for signs in possible_signs_order:
-        output = None
-        max_minus = 0
-        count_minus = 0    
-        for sign in signs:
-            if sign == "-":
-                count_minus +=1
-        if count_minus > max_minus:
-            max_minus = count_minus
-            output = signs
-
-    return "".join(sign for sign in output)
-
 
