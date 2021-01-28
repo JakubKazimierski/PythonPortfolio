962ef5fdf2958897212ea65b99c1981b7d792ff4
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-01-28 04:56:21 +0100

    Simple view template

--- Django/Django_blog/blog/views.py
+++ Django/Django_blog/blog/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'Jakub K',
+        'title' : 'First post',
+        'content' : 'First content',
+        'date_posted' : 'January 28 2021',
+    
+    },
+    {
+        'author' : 'Jakub K',
+        'title' : 'Second post',
+        'content' : 'Second content',
+        'date_posted' : 'January 28 2021',
+    
+    }
+
+]
+
+
 def home(request):
     '''
     View of home page.
     '''
-    return HttpResponse('<h1>Blog Home Page</h1>')
+
+    context = {
+        'posts' : posts
+    }
+
+    return render(request, 'blog/home.html', context)
     
 def about(request):
     '''
     View of description page.
     '''
-    return HttpResponse('<h1>Blog About Page</h1>')    
+    return render(request, 'blog/about.html')
