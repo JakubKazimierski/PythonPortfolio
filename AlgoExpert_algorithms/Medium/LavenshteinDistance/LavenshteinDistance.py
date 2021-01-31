c75831287acda46ea875d86b56cb42ff4c1e2745
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-01-31 14:37:58 +0100

    Improved space complexity

--- AlgoExpert_algorithms/Medium/LavenshteinDistance/LavenshteinDistance.py
+++ AlgoExpert_algorithms/Medium/LavenshteinDistance/LavenshteinDistance.py
@@ -17,8 +17,10 @@ def lavenshteinDistance(str1, str2):
     of a character for another.
     '''
 
-    #time O(n*m) where n is lenght of str1 and m of str2
+    '''
+    # time O(n*m) where n is lenght of str1 and m of str2
     # space O(n*m)
+    # dynamic programming approach
 
     # below contains counting of empty strigns  
     # and makes n*m matrix comparing letters from strings 
@@ -39,8 +41,37 @@ def lavenshteinDistance(str1, str2):
                 edits[row_id][col_id] = 1 + min(edits[row_id][col_id-1], edits[row_id-1][col_id-1], edits[row_id-1][col_id])
 
     return edits[-1][-1]
+    '''
 
+    # time O(n*m) where n is lenght of str1 and m of str2
+    # space O(min(n,m))
+    # dynamic programming approach
+    small_str = str1 if len(str1) < len(str2) else str2
+    big_str = str1 if len(str1) >= len(str2) else str2
 
+    even_edits = [col_id for col_id in range(len(small_str)+1)]
+    odd_edits = [None for col_id in range(len(small_str)+1)]
 
+    for row_id in range(1, len(big_str)+1):
+        if row_id % 2 == 1:
+            current_edits = odd_edits
+            previous_edits = even_edits
+        else:
+            previous_edits = odd_edits
+            current_edits = even_edits
 
+        # below line is evaluation of row in full matrix
+        # 0 <- current_edits[0] in curr iteration
+        # 1 <- current_edits[0] in curr iteration
+        # 2 <- current_edits[0] in curr iteration
+        # 3 <- current_edits[0] in curr iteration
+        # ...
+        current_edits[0] = row_id
 
+        for col_id in range(1, len(small_str)+1):
+            if small_str[col_id-1] == big_str[row_id-1]:
+                current_edits[col_id] = previous_edits[col_id-1]
+            else:
+                current_edits[col_id] = 1 + min(previous_edits[col_id-1],previous_edits[col_id], current_edits[col_id-1])
+
+    return even_edits[-1] if len(big_str) % 2 == 0 else odd_edits[-1]
