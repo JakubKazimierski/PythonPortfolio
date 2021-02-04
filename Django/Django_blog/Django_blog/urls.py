9cd0fc22e3bca7acc9d8ef355c9cea1f50f0d6b6
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-04 16:37:00 +0100

    Register form created

--- Django/Django_blog/Django_blog/urls.py
+++ Django/Django_blog/Django_blog/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('register/', user_views.register, name='register'),
     # empty path for home page
     path('', include('blog.urls')),
 ]
