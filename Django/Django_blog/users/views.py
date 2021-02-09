2dd49907648360bebee6887ecef430f98a41326d
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-10 00:27:24 +0100

    Log In and Log out ability + assertion

--- Django/Django_blog/users/views.py
+++ Django/Django_blog/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -9,9 +10,13 @@ def register(request):
         
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')    
+            messages.success(request, f'Your account has been created! You are able to Log In now.')    
         
-            return redirect('blog-home')
+            return redirect('login')
     else:    
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
