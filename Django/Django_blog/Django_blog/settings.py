9a2f3024b752c92cab5a01869927adcbabbb5ac3
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-09 19:31:50 +0100

    POST form created, and tests

--- Django/Django_blog/Django_blog/settings.py
+++ Django/Django_blog/Django_blog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Django/Django_blog/users/test_views.py
+++ Django/Django_blog/users/test_views.py
@@ -19,13 +19,27 @@ class TestViews(TestCase):
         self.form_url = reverse("register")
 
 
-    def test_home_GET(self):
+    def test_GET_register(self):
         '''
-        GET method for register test.
+        GET method for register, tests.
         '''
-
         response = self.client.get(self.form_url)
 
         self.assertEqual(response.status_code, 200)
         # below looks at dir templates
         self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_POST_register(self):
+        '''
+        POST method for register, tests.
+        '''
+
+        response = self.client.post(self.form_url, {
+             'username' : 'TestsUSers', 
+             'email' : 'email@is.ok',
+             'password1' : 'Testtest321',
+             'password2' : 'Testtest321',
+
+        })
+
+        self.assertEqual(response.status_code, 302)
