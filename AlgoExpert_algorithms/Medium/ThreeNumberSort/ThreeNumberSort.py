ede1dfb2791e089816b4a7c18f76da6e0a2fa0ce
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-09 14:44:17 +0100

    Next approach

--- AlgoExpert_algorithms/Medium/ThreeNumberSort/ThreeNumberSort.py
+++ AlgoExpert_algorithms/Medium/ThreeNumberSort/ThreeNumberSort.py
@@ -27,22 +27,53 @@ def threeNumberSort(array, order):
     all three integers found in the second array--it might only
     contain one or two.
     '''
-
+    # #########################
+    # BUCKET SORT APPROACH
     # O(n) time | O(1) space
-    buckets = [0, 0, 0]
+    # #########################
+    # buckets = [0, 0, 0]
 
-    # below conuts occurences of elements
-    for elem in array:
-       idx_bucket = order.index(elem)
-       buckets[idx_bucket] += 1
+    # # below conuts occurences of elements
+    # for elem in array:
+    #    idx_bucket = order.index(elem)
+    #    buckets[idx_bucket] += 1
 
-    start_idx = end_idx = 0
-    for idx in range(len(order)):
+    # start_idx = end_idx = 0
+    # for idx in range(len(order)):
 
-        end_idx += buckets[idx]
-        # below assigns elements in correct order
-        for elem_idx in range(start_idx, end_idx):
-            array[elem_idx] = order[idx]
-        start_idx += buckets[idx]    
+    #     end_idx += buckets[idx]
+    #     # below assigns elements in correct order
+    #     for elem_idx in range(start_idx, end_idx):
+    #         array[elem_idx] = order[idx]
+    #     start_idx += buckets[idx]    
     
-    return array    
+    # return array    
+
+
+    # #################################
+    # traverse both directions aproach
+    # O(n) time | O(1) space
+    # ##################################
+    firstValue = order[0]
+    thirdValue = order[2]
+
+    # below sort first value
+    first_idx = 0
+    for idx in range(len(array)):
+        if array[idx] == firstValue:
+            array[first_idx], array[idx] = array[idx], array[first_idx]
+            first_idx += 1
+
+    # below sorts last value, so after
+    # that middle value will be sorted too
+    last_idx = len(array)-1
+    for idx in reversed(range(0, len(array))):
+        if array[idx] == thirdValue:
+            array[idx], array[last_idx] = array[last_idx], array[idx]
+            last_idx -= 1
+
+    return array        
+
+
+
+
