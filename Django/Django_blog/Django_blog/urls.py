ecedfd074d1cddb4a5443f40caddbc4a39c35fbe
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-11 00:17:54 +0100

    User profile + pics creation,  implemented

--- Django/Django_blog/Django_blog/urls.py
+++ Django/Django_blog/Django_blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views 
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -27,3 +29,7 @@ urlpatterns = [
     # empty path for home page
     path('', include('blog.urls')),
 ]
+
+# if in debug mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Django/Django_blog/users/signals.py
@@ -0,0 +1,18 @@
+# signal after user is created
+from django.db.models.signals import post_save
+# sender
+from django.contrib.auth.models import User
+# receiver
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
