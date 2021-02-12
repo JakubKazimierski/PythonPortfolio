f71db2cd32ce97ab7ce3ef1fdd077631e0664303
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-12 02:02:52 +0100

    Profil update ability

--- Django/Django_blog/users/models.py
+++ Django/Django_blog/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # one to one relationship
 
@@ -9,3 +10,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- Django/Django_blog/users/views.py
+++ Django/Django_blog/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -19,4 +19,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        prof_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+          
+            messages.success(request, f'Your account has been updated!')    
+            return redirect('profile') 
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        prof_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form' : user_form,
+        'prof_form': prof_form
+    }
+
+    return render(request, 'users/profile.html', context)
