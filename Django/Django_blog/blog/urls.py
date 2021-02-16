ef1d1d8bfa7b33b343f5248758b54240944100a1
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-16 15:24:27 +0100

    Pagination ability implemented

--- Django/Django_blog/blog/urls.py
+++ Django/Django_blog/blog/urls.py
@@ -3,12 +3,13 @@ Contains views.
 '''
 
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     # leave empty path for home page 
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
--- Django/Django_blog/blog/views.py
+++ Django/Django_blog/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
@@ -9,7 +10,19 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # app/model_viewtype.html
     context_object_name = 'posts'
-    oredering = ['-date_posted']
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # app/model_viewtype.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
