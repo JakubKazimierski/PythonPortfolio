907f03be9586afe1a10b1760708089310118b30c
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-02-20 11:26:30 +0100

    Shortner is ready

--- Django/Django_url_shortner/urlshortner/shortner/urls.py
+++ Django/Django_url_shortner/urlshortner/shortner/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<str:pk>', views.go, name='go')
 ]
