70cbd0a3ac5aa21bd8c72a7338aa38497b63d6c7
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-03-04 09:14:34 +0100

    Next challenge

--- AlgoExpert_algorithms/Hard/KnapsackProblem/KnapsackProblem.py
+++ AlgoExpert_algorithms/Hard/KnapsackProblem/KnapsackProblem.py
@@ -25,3 +25,45 @@ def knapsackProblem(items, capacity):
     the total value in the knapsack, your function can return 
     any of them.
     '''
+
+    # Time O(items*capacity) | Space O(items*capacity)
+
+    # 2d array of capacity and items
+    knapsackValues = [[0 for _ in range(0, capacity + 1)] for _ in range(0, len(items) + 1)]
+
+    for idx in range(1, len(items) + 1):
+        currWeight = items[idx - 1][1]
+        currValue = items[idx - 1][0]
+
+        for cap in range(0, capacity + 1):
+            # previous max value
+            if currWeight > cap:
+                knapsackValues[idx][cap] = knapsackValues[idx - 1][cap]
+            # new max value
+            else:
+                knapsackValues[idx][cap] = max(knapsackValues[idx-1][cap], knapsackValues[idx-1][cap-currWeight] + currValue)
+        
+    return [knapsackValues[-1][-1], getKnapsackItems(knapsackValues, items)]
+
+def getKnapsackItems(knapsackValues, items):
+    '''
+    Returns indexes of packed to knapsack items            
+    Time O(items*capacity) | Space O(1)
+    '''
+
+    sequence_items = []
+    idx = len(knapsackValues) - 1
+    cap = len(knapsackValues[0]) - 1
+
+    while idx > 0:
+        if knapsackValues[idx][cap] == knapsackValues[idx-1][cap]:
+            idx -= 1
+        else:
+            # due to extra index in 2d array decrement
+            sequence_items.append(idx-1)
+            cap -= items[idx-1][1]
+            idx -= 1
+        if cap == 0:
+            break
+    return list(reversed(sequence_items))        
+
