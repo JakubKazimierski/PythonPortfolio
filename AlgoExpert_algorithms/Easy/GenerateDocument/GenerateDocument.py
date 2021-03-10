26356136ecb231c1650b4ccb2aa6b0e59a97271b
JakubKazimierski <j.m.kazimierski@gmail.com>
2021-03-10 23:19:58 +0100

    Description

--- AlgoExpert_algorithms/Easy/GenerateDocument/GenerateDocument.py
+++ AlgoExpert_algorithms/Easy/GenerateDocument/GenerateDocument.py
@@ -4,5 +4,24 @@ March 2021 Jakub Kazimierski
 '''
 
 def generateDocument(characters, document):
-    # Write your code here.
-    return False
+    '''
+    You're given a string of available charaacters
+    and a string representing a document that you need
+    to generate. Write a function that determines if 
+    you can generate the document using the available
+    characters. If you can generate the document, your function
+    should return True; otherwise it should return False.
+
+    You're only able to generate the document if the frequency
+    of unique characters in the characters string is greater
+    tha or equal to the frequency of unique characters in the document
+    string. For examle, if you're given characters = "abcabc"
+    and document = "aabbccc" you cannot generate the document because
+    you're missing one c.
+
+    The document that you need to create may contain any characters,
+    including special characters, capital letters, numbers
+    and spaces. 
+
+    Note you can aways generate the empty string ("")
+    '''
